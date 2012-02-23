7b4002c3011e2c23de60056d65da42a2fdbef950
Chris Maddox <tyre77@gmail.com>
2012-02-23 10:58:10 -0500

    rails app

---
+++ Taxi_Rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaxiRails::Application.config.secret_token = 'f0f91f3b95314065a71f99e9fe402d1deb930a4e6add1dd491565a62fe62676061b55aba6d2e65df9046a327e0108b0383099bc0c097dfb4fc9a0c6e6820f307'
