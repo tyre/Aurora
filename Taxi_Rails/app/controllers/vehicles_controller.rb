35c59ced26478aa0d07b775991f2b35b627e46b9
Chris Maddox <tyre77@gmail.com>
2012-02-23 14:13:49 -0500

    tons of Vehicle controller and view additions!

--- Taxi_Rails/app/controllers/vehicles_controller.rb
+++ Taxi_Rails/app/controllers/vehicles_controller.rb
@@ -1,21 +1,37 @@
 class VehiclesController < ApplicationController
-def index
-  @vehicles = Vehicle.all
-end
+  def index
+    @vehicles = Vehicle.all
+  end
 
-def new
-  @vehicle = Vehicle.new
-end
+  def new
+    @vehicle = Vehicle.new
+  end
 
-def show
-  @vehicle = Vehicle.find(params[:id])
-end
+  def show
+    @vehicle = Vehicle.find(params[:id])
+    begin
+      @driver = Driver.find(@vehicle)
+    rescue
+      @driver = nil
+    end
+  end
 
-def create
-  @vehicle = Vehicle.new(params[:vehicle])
-  @vehicle.save
-  flash[:message] = "Created vehicle with id #{@vehicle.id}!"
-  redirect_to vehicle_path(@vehicle)
-end
+  def create
+    @vehicle = Vehicle.new(params[:vehicle])
+    @vehicle.save
+    flash[:message] = "Created vehicle with id #{@vehicle.id}!"
+    redirect_to vehicle_path(@vehicle)
+  end
+
+  def edit
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def update
+    @vehicle = Vehicle.find(params[:id])
+    @vehicle.update_attributes(params[:vehicle])
+    flash[:message] = "Vehicle #{@vehicle.id} updated!"
+    redirect_to vehicle_path(@vehicle)
+  end
 
 end
